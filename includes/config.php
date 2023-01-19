98e5a0606c7d40dd16dc0f0a081c78b6c5db3cf4
JuliaLabre <112015111+JuliaLabre@users.noreply.github.com>
2023-01-19 15:54:32 -0300

    Config

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+//  Ligação com o banco de dados
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "venushop";
+$port = 3306;
+
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=".$dbname,$user,$pass);
+    echo "Conexão com banco de dados realizado com sucesso!";
+} catch(PDOExcepcion $erro) {
+   echo "Erro: Conexão com o banco de dados não realizada".$erro;
+}
